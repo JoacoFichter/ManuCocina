7a2b66224531ad585f3fd847ecd214a13be51aa8
JoacoFichter <joaquinfichter9@gmail.com>
2021-10-19 21:53:49 -0300

    Agregué objetos para el desafío de la clase 5

--- views/productos.js
+++ views/productos.js
@@ -7,4 +7,26 @@ function comprar() {
     calcularIva(valorProducto);
 }
 
+class producto {
+    constructor(nombre, precio, cantidadEnStock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidadEnStock = cantidadEnStock;
+    }
+    mostrarPrecio() {
+        alert("El precio del producto es de $" + this.precio + " por unidad");
+    }
+    disponibilidad(cantidadAComprar = prompt("Cuántos " + this.nombre + " desea comprar?")) {
+        if (cantidadAComprar <= this.cantidadEnStock) {
+            alert("El producto está disponible");
+            this.mostrarPrecio();
+            alert("el precio total es de $" + (parseInt(cantidadAComprar) * this.precio));
+        }else {
+            alert("El producto no está disponible");
+        }
+    }
+}
+
+const gallesChoco = new producto("Galletitas de chocolate", 150, 10);
+const gallesCrackers = new producto("Galletitas Crackers", 120, 8);
 
