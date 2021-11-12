82c2673b0e2ca4326d803daf975ac4a29329961b
JoacoFichter <joaquinfichter9@gmail.com>
2021-11-12 00:03:06 -0300

    incorporación de Jquery

--- desafioDom.js
+++ desafioDom.js
@@ -1,7 +1,10 @@
 // DESAFIO DOM
 
 let nombreUsuario = prompt("Cuál es tu nombre?");
+/*
 let titulosIndex = document.getElementById("titulosIndex");
 let presentacion = document.createElement("div");
 presentacion.innerHTML = `<h2 style="display: flex; justify-content: center;">Hola ${nombreUsuario}!</h3>`;
 titulosIndex.appendChild(presentacion); 
+*/
+$("#titulosIndex").append(`<h2 style="display: flex; justify-content: center;">Hola ${nombreUsuario}!</h3>`); 
--- views/main.js
+++ views/main.js
@@ -1,4 +1,4 @@
-//declaración de objetos, incorporación de los mismos a Array "listaProductos" y guardado en el localStorage:
+//declaración de objetos, incorporación de los mismos a Array "listaProductos":
 
 class producto {
     constructor(nombre, precio, cantidadEnStock, nombreIMG) {
@@ -22,86 +22,62 @@ class producto {
 }
 
 const listaProductos = [];
-const gallesChoco = new producto("Galletitas de chocolate", 150, 10, "gallesChoco");
-const gallesCrackers = new producto("Galletitas Crackers", 120, 8, "gallesCrackers");
-const gallesAvena = new producto("Galletitas de avena", 120, 10, "gallesAvena");
-const gallesLimon = new producto("Galletitas de limón", 140, 50, "gallesLimon");
-const budinVainilla = new producto("Budín de vainilla", 300, 5, "budinVainilla");
-const budinChoco = new producto("Budín de chocolate", 350, 9, "budinChoco");
-const budinLimon = new producto("Budín de limón", 280, 7, "budinLimon");
-const budinMarmolado = new producto("Budín marmolado", 300, 5, "budinMarmolado");
-const bizcochuelo = new producto("Bizcochuelo", 500, 10, "bizcochuelo");
-const marquise = new producto("Marquise de chocolate", 700, 4, "marquise");
-const pastafrola = new producto("Pastafrola", 550, 2, "pastafrola");
-const tortaVainilla = new producto("Torta de Vainilla", 460, 8, "tortaVainilla");
+const gallesChoco = new producto("Galletitas de chocolate", 120, 10, "gallesChoco");
+const gallesCrackers = new producto("Galletitas Crackers", 130, 8, "gallesCrackers");
+const gallesAvena = new producto("Galletitas de avena", 140, 10, "gallesAvena");
+const gallesLimon = new producto("Galletitas de limón", 145, 50, "gallesLimon");
+const budinVainilla = new producto("Budín de vainilla", 280, 5, "budinVainilla");
+const budinChoco = new producto("Budín de chocolate", 290, 9, "budinChoco");
+const budinLimon = new producto("Budín de limón", 300, 7, "budinLimon");
+const budinMarmolado = new producto("Budín marmolado", 350, 5, "budinMarmolado");
+const bizcochuelo = new producto("Bizcochuelo", 400, 10, "bizcochuelo");
+const marquise = new producto("Marquise de chocolate", 450, 4, "marquise");
+const pastafrola = new producto("Pastafrola", 500, 2, "pastafrola");
+const tortaVainilla = new producto("Torta de Vainilla", 550, 8, "tortaVainilla");
 
-listaProductos.push (gallesChoco, gallesCrackers, gallesAvena, gallesLimon, budinChoco, budinLimon, budinMarmolado, budinVainilla, bizcochuelo, marquise, pastafrola, tortaVainilla);
+listaProductos.push (gallesChoco, gallesCrackers, gallesAvena, gallesLimon, budinVainilla, budinChoco, budinLimon, budinMarmolado, bizcochuelo, marquise, pastafrola, tortaVainilla);
 
-const guardarLocal = (clave, valor) => {localStorage.setItem(clave, valor)};
+//guardado del array en el localStorage:
+
+let guardarLocal = (clave, valor) => {localStorage.setItem(clave, valor)};
 guardarLocal("listaProductos", JSON.stringify(listaProductos));
+
 let datosAlmacenados = JSON.parse(localStorage.getItem("listaProductos"));
-console.log(datosAlmacenados);
 
-//generación de cards a través del uso del DOM:
+//generación de cards a través de la manipulación del DOM:
 
-for (let producto of datosAlmacenados) {
-    let flexCards = document.getElementById("flexCards");
-    let crearCard = document.createElement("div");
-    crearCard.innerHTML = `<div id="${producto.nombre}" class="card" style="height: 350px; width: 18rem; margin: 40px;">
-                                <img src="../assets/img/${producto.nombreIMG}.jpg" class="card-img-top" alt="${producto.nombre}">
-                                <div class="card-body">
-                                    <h5 class="card-title">${producto.nombre}</h5>
-                                    <p>$${producto.precio}</p>
-                                    <a href="#" class="btn btn-primary">Añadir al carrito</a>
-                                </div>
-                                <div id="cantidadEnCarrito"></div>
-                            </div>`
-    flexCards.appendChild(crearCard);
-}
-
-//variables y funciones para el Filtrado:  (DESAFIO "EVENTOS")
-
-let productosFiltrados = listaProductos;
-let boton = document.getElementById("btnFiltrar");
-boton.addEventListener("click", filtrarDatos);
-let textoAFiltrar = document.getElementById("textoAFiltrar");
-let divCards = document.getElementById("flexCards");
-let padre = document.getElementById("flexCardsPadre");
-
-const limpiarHTML = () =>{
-    padre.removeChild(divCards);
-    divCards = document.createElement("div");
-    let a = document.createAttribute("id");
-    a.value = "flexCards";
-    divCards.setAttributeNode(a);
-    let b = document.createAttribute("class");
-    b.value = "colFlex colFlex--alignCenter colFlex--justifyCenter flexWrap flexSpcAround";
-    divCards.setAttributeNode(b);
-    padre.appendChild(divCards);
-}
-
-const mostrarProductos = () => {
-    for (const producto of productosFiltrados){
-        let crearCard = document.createElement("div");
-    crearCard.innerHTML = `<div id="${producto.nombre}" class="card" style="height: 350px; width: 18rem; margin: 40px;">
-                                <img src="../assets/img/${producto.nombreIMG}.jpg" class="card-img-top" alt="${producto.nombre}">
-                                <div class="card-body">
-                                    <h5 class="card-title">${producto.nombre}</h5>
-                                    <p>$${producto.precio}</p>
-                                    <a href="#" class="btn btn-primary">Añadir al carrito</a>
-                                </div>
-                                <div id="cantidadEnCarrito"></div>
-                            </div>`
-    flexCards.appendChild(crearCard);
+let crearCards = (lista) => {
+    for (let producto of lista) {
+        $("#flexCards").append(
+                        `<div id="${producto.nombre}" class="card" style="height: 350px; width: 18rem; margin: 40px;">
+                            <img src="../assets/img/${producto.nombreIMG}.jpg" class="card-img-top" alt="${producto.nombre}">
+                            <div class="card-body">
+                                <h5 class="card-title">${producto.nombre}</h5>
+                                <p>$${producto.precio}</p>
+                                <a href="#" class="btn btn-primary">Añadir al carrito</a>
+                            </div>
+                            <div id="cantidadEnCarrito"></div>
+                        </div>`
+        );
     }
 }
 
-function filtrarDatos(){
-    productosFiltrados = listaProductos.filter(elemento => (elemento.nombre).toUpperCase().includes((textoAFiltrar.value).toUpperCase()));
-    limpiarHTML();
-    mostrarProductos();
+crearCards(datosAlmacenados);
+
+//variables y funciones para el Filtrado:  (DESAFIO "EVENTOS")
+
+const limpiarHTML = () =>{
+    $("#flexCards").remove();
+    $("#flexCardsPadre").prepend(`<div id="flexCards" class="colFlex colFlex--alignCenter colFlex--justifyCenter flexWrap flexSpcAround"></div>`);
 }
 
+let filtrarDatos = () => {
+    productosFiltrados = listaProductos.filter(elemento => (elemento.nombre).toUpperCase().includes((($("#textoAFiltrar")[0]).value).toUpperCase()));
+    limpiarHTML();
+    crearCards(productosFiltrados);
+}
+
+$("#btnFiltrar").click(filtrarDatos);
 
 
 
